8c4f869c92b99350cc4e29720dce051af1a745b3
pinky liwanagan <pliwanagan@hotmail.com>
2014-09-24 15:16:00 +0800

    remove extra bracket

--- Align.py
+++ Align.py
@@ -15,30 +15,29 @@ def main():
 	# print mod.product('info','c0wtkZonrfeXWTzayAHY')
 	# print mod.product('create',create_product_data)
 
-	create_invoice_data = {
-		'checkout_type' : 'btc',
-		'products' : [{
-			'product_name' 		: 'Invoice prod1',
-			'product_price' 	: '1',
-			'quantity' 			: '1',
-			'product_shipping' 	: '1'
-		}]
-		[],
-		'buyer_info'	: {
-			'first_name' : 'Invoice buyer1',
-			'last_name'  : 'Invoice buyer1',
-			'email'		 : 'invoicebuyer1@mail.com',
-			'address_1'  : 'Invoice buyer1',
-			'address_2'  : 'Invoice buyer1'
-		}
-	}
+	# create_invoice_data = {
+	# 	'checkout_type' : 'btc',
+	# 	'products' : [{
+	# 		'product_name' 		: 'Invoice prod1',
+	# 		'product_price' 	: '1',
+	# 		'quantity' 			: '1',
+	# 		'product_shipping' 	: '1'
+	# 	}],
+	# 	'buyer_info'	: {
+	# 		'first_name' : 'Invoice buyer1',
+	# 		'last_name'  : 'Invoice buyer1',
+	# 		'email'		 : 'invoicebuyer1@mail.com',
+	# 		'address_1'  : 'Invoice buyer1',
+	# 		'address_2'  : 'Invoice buyer1'
+	# 	}
+	# }
 	# print type(create_invoice_data)
 	# sys.exit()
 	# print type(create_invoice_data)
 
-	# print mod.invoice('list')
+	print mod.invoice('list')
 	# print mod.invoice('info','DkUph8nMYr4pySINqaDD')
-	print mod.invoice('create',create_invoice_data)
+	# print mod.invoice('create',create_invoice_data)
 
 if __name__ == "__main__":
     main()
